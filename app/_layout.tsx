433d3e312c539804aeaafde766a1054ba62c5851
Styles <stylesk11@gmail.com>
2024-08-29 13:03:28 -0700

    add path aliasing

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,34 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { theme } from "@/theme";
 
 export default function Layout() {
   return (
-    <Stack>
-      <Stack.Screen
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: theme.colors.green,
+      }}
+    >
+      <Tabs.Screen
         name="index"
         options={{
           title: "Home",
-        }} />
-    </Stack>
-  )
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="leaf" color={color} size={size} />
+          ),
+          tabBarShowLabel: false,
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="user" color={color} size={size} />
+          ),
+          tabBarShowLabel: false,
+        }}
+      />
+    </Tabs>
+  );
 }
