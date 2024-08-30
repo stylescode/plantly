cf416a8fe683c6ffa8a775f7fe5a4056a3a9eeed
Styles <stylesk11@gmail.com>
2024-08-30 14:54:45 -0700

    persist onboarding

--- store/userStore.ts
+++ store/userStore.ts
@@ -1,15 +1,25 @@
 import { create } from "zustand";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 type UserStore = {
   hasFinishedOnboarding: boolean;
   toggleOnboarding: () => void;
 };
 
-export const useUserStore = create<UserStore>((set) => ({
-  hasFinishedOnboarding: false,
-  toggleOnboarding: () =>
-    set((state) => ({
-      ...state,
-      hasFinishedOnboarding: !state.hasFinishedOnboarding,
-    })),
-}));
+export const useUserStore = create(
+  persist<UserStore>(
+    (set) => ({
+      hasFinishedOnboarding: false,
+      toggleOnboarding: () =>
+        set((state) => ({
+          ...state,
+          hasFinishedOnboarding: !state.hasFinishedOnboarding,
+        })),
+    }),
+    {
+      name: "plantlyUserStore",
+      storage: createJSONStorage(() => AsyncStorage),
+    },
+  ),
+);
