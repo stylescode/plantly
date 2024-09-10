46435c0cde5cfca0a3de8d816a7149cca6372439
styles <styles@Melissas-Air.lan>
2024-09-09 17:10:31 -0700

    add quick action

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,25 @@
 import { Stack } from "expo-router";
+import { useEffect } from "react";
+import * as QuickActions from "expo-quick-actions";
+import { Platform } from "react-native";
+import { useQuickActionRouting } from "expo-quick-actions/router";
 
 export default function Layout() {
+  useQuickActionRouting();
+
+  useEffect(() => {
+    QuickActions.setItems([
+      {
+        title: "Add plant",
+        icon: Platform.OS === "ios" ? "symbol:leaf" : "leaf",
+        id: "0",
+        params: {
+          href: "/newPlant",
+        },
+      },
+    ]);
+  }, []);
+
   return (
     <Stack>
       <Stack.Screen
